b28fc674f0c94bd67b41487b566fd1bd1527adb6
Laura Molina <ld.molina11@uniandes.edu.co>
2024-09-11 00:58:36 -0500

    :sparkles: refactor ms and bd connection

--- get_incident/app.py
+++ get_incident/app.py
@@ -1,55 +1,72 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask
+from blueprints import BlueprintHealth, BlueprintIncidents
+from repositories import IncidentRepository
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud import secretmanager
 from dotenv import load_dotenv
+import json
 
-# Cargar las variables de entorno del archivo .env
-load_dotenv()
+API_PREFIX = "/v1/incidents"
 
-
-app = Flask(__name__)
+load_dotenv("../.env")
 
 
 def access_secret_version(secret_id, version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-
-    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    response = client.access_secret_version(name=name)
-    secret_payload = response.payload.data.decode("UTF-8")
-
-    return secret_payload
-
-
-# Obtener las credenciales desde Google Secret Manager
-service_account_info = access_secret_version("FIREBASE_SERVICE_ACCOUNT")
-
-# Obtener el nombre de la colección desde Google Secret Manager
-collection_name = access_secret_version("FIRESTORE_COLLECTION_NAME")
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-
-@app.route("/incidents/<string:doc_id>", methods=["GET"])
-def get_data(doc_id):
     try:
-        doc_ref = db.collection(collection_name).document(doc_id)
-        doc = doc_ref.get()
+        client = secretmanager.SecretManagerServiceClient()
+        project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
-        if doc.exists:
-            return jsonify(doc.to_dict()), 200
-        else:
-            return jsonify({"error": "Documento no encontrado"}), 404
+        if not project_id:
+            raise EnvironmentError("GOOGLE_CLOUD_PROJECT no está configurado.")
 
+        print(f"project_id: {project_id}", flush=True)
+
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+        response = client.access_secret_version(name=name)
+        secret_payload = response.payload.data.decode("UTF-8")
+        return secret_payload
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        print(f"Error al acceder al secreto {secret_id}: {e}", flush=True)
+        raise
+
+
+def create_app():
+    app = Flask(__name__)
+
+    try:
+        service_account_info = access_secret_version("firebase_service_account")
+        service_account_info = json.loads(service_account_info)
+    except Exception as e:
+        print(f"Error al obtener credenciales de Firebase: {e}", flush=True)
+        raise
+
+    try:
+        collection_name = access_secret_version("firestore_collection_name")
+    except Exception as e:
+        print(f"Error al obtener el nombre de la colección: {e}", flush=True)
+        raise
+
+    try:
+        cred = credentials.Certificate(service_account_info)
+        if not firebase_admin._apps:
+            firebase_admin.initialize_app(cred)
+    except Exception as e:
+        print(f"Error al inicializar Firebase: {e}", flush=True)
+        raise
+
+    db = firestore.client()
+
+    app.repositories = {IncidentRepository: IncidentRepository(db, collection_name)}
+
+    app.register_blueprint(BlueprintHealth)
+    app.register_blueprint(BlueprintIncidents, url_prefix=API_PREFIX)
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app = create_app()
+    app.run(host="127.0.0.1", port=8080, debug=True)
